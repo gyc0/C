407ddb79fe24abbcb2fef5d64eacc90001f6f951
gyc0 <78998731+gyc0@users.noreply.github.com>
2024-09-29 20:20:13 +0800

    Different answer with different compiler
    This code got  different answer with different compiler， in Clion(Jetbrain) got 100. In VStudio got 105?

---
+++ main.c
@@ -0,0 +1,22 @@
+//
+// Created by gywxm on 2024/3/14.
+//
+#include <stdio.h>
+
+/*int main(){
+    char a='\165';
+    printf("%c",a);
+    return 0;
+
+}*/
+int main(){
+    int b;
+    char a='a';
+    int j=5;
+    int i=3;
+    float x=2.5;
+    double y=5.0,z;
+    i= a=a+i,a+j;
+    printf("%d",i );
+    return 0;
+}
